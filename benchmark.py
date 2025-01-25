60f9ea847aa8099b644b16d0b4024beb69d18c8d
Karim Ibrahim <karim.ibrahim93@gmail.com>
2025-01-25 15:24:33 +0100

    adjust benchmark

--- benchmark.py
+++ benchmark.py
@@ -177,71 +177,75 @@ def simulation_p_vectors():
     f_max = 2
 
     
-    k = 32
-    k_prime = 32
+    k = 4
+    k_prime = 4
     G = UQFE(group, p_order, g1, g2, gt, k, k_prime, lamda)
+    message_length = [16,32,64,128]
+    for l in message_length:
+        n1 = l
+        n2 = n1
+        z_1 = random_vector(0, z_1_max, n1)
+        z_2 = random_vector(0, z_2_max, n2)
+        Iz_1 = [i for i in range(1, n1 + 1)]
+        Iz_2 = [i for i in range(1, n2 + 1)]
+        f = random_vector(1, f_max, n1 * n2)
+        If_1 = Iz_1
+        If_2 = Iz_2
 
-    n1 = 200
-    n2 = n1
-    z_1 = random_vector(0, z_1_max, n1)
-    z_2 = random_vector(0, z_2_max, n2)
-    Iz_1 = [i for i in range(1, n1 + 1)]
-    Iz_2 = [i for i in range(1, n2 + 1)]
-    f = random_vector(1, f_max, n1 * n2)
-    If_1 = Iz_1
-    If_2 = Iz_2
+        start_time = time.time()
+        pp, msk = G.setup(p_order)
+        setup_time = time.time() - start_time
 
-    start_time = time.time()
-    pp, msk = G.setup(p_order)
-    setup_time = time.time() - start_time
+        start_time = time.time()
+        CT, CT_Plain = G.encrypt(pp, msk, z_1, z_2, Iz_1, Iz_2)
+        encrypt_time = time.time() - start_time
 
-    start_time = time.time()
-    CT, CT_Plain = G.encrypt(pp, msk, z_1, z_2, Iz_1, Iz_2)
-    encrypt_time = time.time() - start_time
+        start_time = time.time()
+        skf, skf_plain = G.keygen(pp, msk, f, If_1, If_2)
+        keygen_time = time.time() - start_time
 
-    start_time = time.time()
-    skf, skf_plain = G.keygen(pp, msk, f, If_1, If_2)
-    keygen_time = time.time() - start_time
+        start_time = time.time()
+        v = G.decrypt(pp, skf_plain, CT_Plain)
+        decrypt_time = time.time() - start_time
 
-    start_time = time.time()
-    v = G.decrypt(pp, skf_plain, CT_Plain)
-    decrypt_time = time.time() - start_time
+        setup_time *= 1_000_000_000
+        keygen_time *= 1_000_000_000
+        encrypt_time *= 1_000_000_000
+        decrypt_time *= 1_000_000_000
+        total_time = setup_time + keygen_time + encrypt_time + decrypt_time
 
-    setup_time *= 1_000_000_000
-    keygen_time *= 1_000_000_000
-    encrypt_time *= 1_000_000_000
-    decrypt_time *= 1_000_000_000
-    total_time = setup_time + keygen_time + encrypt_time + decrypt_time
+        expected = G.get_expected_result(p_order, z_1, f, z_2)
+        print("expected result: ", expected)
+        print("calculated result: ", v)
+        s_msk = size_in_kilobits(msk)
+        s_pp = size_in_kilobits(pp)
+        s_ct = size_in_kilobits(CT)
+        s_sk = size_in_kilobits(skf)
 
-    expected = G.get_expected_result(p_order, z_1, f, z_2)
-    print("expected result: ", expected)
-    print("calculated result: ", v)
-    s_msk = size_in_kilobits(msk)
-    s_pp = size_in_kilobits(pp)
-    s_ct = size_in_kilobits(CT)
-    s_sk = size_in_kilobits(skf)
+        results.append(
+            [
+                n1,
+                n2,
+                k,
+                k_prime,
+                lamda,
+                s_msk,
+                s_pp,
+                s_ct,
+                s_sk,
+                setup_time,
+                keygen_time,
+                encrypt_time,
+                decrypt_time,
+                total_time,
+            ]
+        )
 
-    results.append(
-        [
-            k,
-            k_prime,
-            lamda,
-            s_msk,
-            s_pp,
-            s_ct,
-            s_sk,
-            setup_time,
-            keygen_time,
-            encrypt_time,
-            decrypt_time,
-            total_time,
-        ]
-    )
-
-    with open("data/uqfe_benchmark_p_vectors_200.csv", "w", newline="") as csvfile:
+    with open("data/uqfe_benchmark_message_lengths}.csv", "w", newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(
-            [
+            [   "n1",
+                "n2",
                 "k",
                 "k_prime",
                 "lamda",
